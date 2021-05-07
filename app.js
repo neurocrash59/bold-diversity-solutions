f9b6c751b05c83eaecc5d9011dbfcc5bbe7cfc06
neurocrash59 <79650435+neurocrash59@users.noreply.github.com>
2021-05-08 00:55:41 +0700

    Clickwrap changes

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 AOS.init();
+
+const acceptTermsCheckbox = document.querySelector('#acceptTermsCheck');
+
+acceptTermsCheckbox.addEventListener('click', function () {
+    document.querySelector('#acceptTermsBtn').disabled = false;
+})
